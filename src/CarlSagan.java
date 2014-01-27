830a1134cc65c8ae2cd042a5a20922ca4d4b4265
pieteradejong <pieter.a.dejong@gmail.com>
2014-01-26 23:14:06 -0800

    added great Sagan quote

---
+++ src/CarlSagan.java
@@ -0,0 +1,12 @@
+
+public class CarlSagan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("A book is proof that humans are capable of working magic. ~Carl Sagan");
+
+	}
+
+}
